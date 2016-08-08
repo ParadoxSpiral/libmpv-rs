b33446efdf973eaee0b8252ee975edf4ab908c1a
ParadoxSpiral <ParadoxSpiral@riseup.net>
2016-08-08 21:14:24 +0200

    Remove bloat in example

--- examples/src/events.rs
+++ examples/src/events.rs
@@ -28,8 +28,6 @@ use std::thread;
 pub fn exec() {
     let mpv = UninitializedParent::new(true).unwrap();
     mpv.set_option(&mut Property::new("cache-initial", Data::new(1))).unwrap();
-    // mpv.set_option(&mut Property::new("ao-volume", Data::new(100))).unwrap();
-    // mpv.set_option(&mut Property::new("ao-mute", Data::new(false)));
     mpv.set_option(&mut Property::new("volume", Data::new(10))).unwrap();
     mpv.set_option(&mut Property::new("no-video", Data::new(true))).unwrap();
     mpv.set_option(&mut Property::new("ytdl", Data::new(true))).unwrap();
