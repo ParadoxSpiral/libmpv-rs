29aed3295cd30ece6bc81913ed86c17249e64a79
Mukund Lakshman <yaymukund@gmail.com>
2020-07-15 11:28:25 +0100

    Add PhantomData for Iterator types, enum accessor methods.

--- src/tests.rs
+++ src/tests.rs
@@ -160,31 +160,27 @@ fn node_map() -> Result<()> {
 
     thread::sleep(Duration::from_millis(250));
     let audio_params: MpvNode = mpv.get_property("audio-params")?;
+    let params: HashMap<&str, MpvNode> =
+        audio_params.to_map().ok_or_else(|| Error::Null)?.collect();
 
-    if let MpvNodeValue::Map(data) = audio_params.value()? {
-        let params: HashMap<&str, MpvNode> = data.collect();
+    assert_eq!(params.len(), 5);
 
-        assert_eq!(params.len(), 5);
+    let format = params.get("format").unwrap().value()?;
+    assert!(matches!(format, MpvNodeValue::String("s16")));
 
-        let format = params.get("format").unwrap().value()?;
-        assert!(matches!(format, MpvNodeValue::String("s16")));
+    let samplerate = params.get("samplerate").unwrap().value()?;
+    assert!(matches!(samplerate, MpvNodeValue::Int64(48_000)));
 
-        let samplerate = params.get("samplerate").unwrap().value()?;
-        assert!(matches!(samplerate, MpvNodeValue::Int64(48_000)));
+    let channels = params.get("channels").unwrap().value()?;
+    assert!(matches!(channels, MpvNodeValue::String("mono")));
 
-        let channels = params.get("channels").unwrap().value()?;
-        assert!(matches!(channels, MpvNodeValue::String("mono")));
+    let hr_channels = params.get("hr-channels").unwrap().value()?;
+    assert!(matches!(hr_channels, MpvNodeValue::String("mono")));
 
-        let hr_channels = params.get("hr-channels").unwrap().value()?;
-        assert!(matches!(hr_channels, MpvNodeValue::String("mono")));
+    let channel_count = params.get("channel-count").unwrap().value()?;
+    assert!(matches!(channel_count, MpvNodeValue::Int64(1)));
 
-        let channel_count = params.get("channel-count").unwrap().value()?;
-        assert!(matches!(channel_count, MpvNodeValue::Int64(1)));
-
-        Ok(())
-    } else {
-        Err(Error::Raw(mpv_error::UnknownFormat))
-    }
+    Ok(())
 }
 
 #[test]
@@ -199,22 +195,17 @@ fn node_array() -> Result<()> {
 
     thread::sleep(Duration::from_millis(250));
     let playlist: MpvNode = mpv.get_property("playlist")?;
+    let items: Vec<MpvNode> = playlist.to_array().ok_or_else(|| Error::Null)?.collect();
 
-    match playlist.value()? {
-        MpvNodeValue::Array(data) => {
-            let items: Vec<MpvNode> = data.collect();
-            assert_eq!(items.len(), 1);
+    assert_eq!(items.len(), 1);
+    let track: HashMap<&str, MpvNode> = items[0].to_map().ok_or_else(|| Error::Null)?.collect();
 
-            match items[0].value()? {
-                MpvNodeValue::Map(data) => {
-                    let track: HashMap<&str, MpvNode> = data.collect();
-                    let filename = track.get("filename").unwrap().value()?;
-                    assert!(matches!(filename, MpvNodeValue::String("test-data/speech_12kbps_mb.wav")));
-                    Ok(())
-                }
-                _ => Err(Error::Raw(mpv_error::UnknownFormat)),
-            }
-        }
-        _ => Err(Error::Raw(mpv_error::UnknownFormat)),
-    }
+    let filename = track.get("filename").unwrap().value()?;
+
+    assert!(matches!(
+        filename,
+        MpvNodeValue::String("test-data/speech_12kbps_mb.wav")
+    ));
+
+    Ok(())
 }
